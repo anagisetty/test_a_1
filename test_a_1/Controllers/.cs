de6b520df9c55dc6f7031f8be7e906436f1e9733
dotnet-code <@dotnet-code>
2023-04-19 10:39:23 +0530

    commit from code202304193923

---
+++ test_a_1/Controllers/.cs
@@ -0,0 +1,59 @@
+﻿Project: To create a REST API for managing user profiles
+User Stories:
+
+1. As a user, I want to be able to create my profile 
+2. As a user, I want to be able to update my profile 
+3. As a user, I want to be able to delete my profile 
+
+Using .NET Core, the following code can be used to create a basic REST API for managing user profiles:
+
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace test_a_1
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        //Declare a list to store the user profile
+        private static List<User> userProfileList = new List<User>();
+
+        // GET: api/User
+        [HttpGet]
+        public IEnumerable<User> Get()
+        {
+            return userProfileList;
+        }
+
+        // POST: api/User
+        [HttpPost]
+        public void Post([FromBody] User user)
+        {
+            userProfileList.Add(user);
+        }
+
+        // PUT: api/User/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] User user)
+        {
+            userProfileList[id] = user;
+        }
+
+        // DELETE: api/User/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            userProfileList.RemoveAt(id);
+        }
+    }
+
+    public class User
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string ProfilePictureUrl { get; set; }
+    }
+
+}
---
+++ test_a_1/Controllers/HistoryController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Data.SqlClient;
+
+namespace test_a_1.Controllers
+{
+    public class HistoryController : ApiController
+    {
+        [HttpGet]
+        [Route("api/history/gethistory")]
+        public IHttpActionResult GetHistory()
+        {
+            List<History> historyList = new List<History>();
+
+            //calling java API to get history
+            string apiURL = "http://example.com/api/history";
+            using (WebClient webClient = new WebClient())
+            {
+                var response = webClient.DownloadString(apiURL);
+                historyList = JsonConvert.DeserializeObject<List<History>>(response);
+            }
+
+            //Fetching request information from DB
+            string connectionString = "Data Source=ServerName;Initial Catalog=DBName;User Id=UserName;Password=PassWord;";
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("SELECT * FROM Requests", con);
+                con.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    historyList.Add(new History
+                    {
+                        RequestId = reader["RequestId"].ToString(),
+                        RequestName = reader["RequestName"].ToString(),
+                        RequestStatus = reader["RequestStatus"].ToString()
+                    });
+                }
+            }
+
+            return Ok(historyList);
+        }
+    }
+
+    public class History
+    {
+        public string RequestId { get; set; }
+        public string RequestName { get; set; }
+        public string RequestStatus { get; set; }
+    }
+}
